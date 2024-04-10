1ac48aa344320336693181a85cb7dd0ff5a67993
David Vazquez <dave_u@outlook.com>
2024-04-09 22:16:19 -0600

    first commit

---
+++ src/presentation/server.ts
@@ -0,0 +1,20 @@
+import { CheckServive } from "../domain/use-cases/checks/check-service";
+import { CronService } from "./cron/cron-service";
+
+export class Server {
+    public static start() {
+        console.log("Server started...");
+
+        CronService.createJob(
+            "*/5 * * * * *",
+            () => {
+                const url = "https://google.com"
+                new CheckServive(
+                    () => console.log( `${ url } is ok` ),
+                    ( error ) => console.log( error ),
+                ).execute("https://google.com");
+            }
+        );
+
+    }
+}
